4766e91f0dc24723aa20ab59da617a59ac9446f6
ju8ko <julia.x.kobzar@gmail.com>
2022-07-18 13:37:34 +0200

    Add sum.js

--- howManyArgs.js
+++ howManyArgs.js
@@ -1,9 +1,15 @@
 // Create a function howManyArgs which returns the total amount of arguments passed to it.
 // Example: passing 3 arguments when calling the function should return the number 3, passing 15 arguments should return the umber 15
+//1
 function howManyArgs() {
   return arguments.length;
 }
-
 console.log(howManyArgs()); // -> 0
 console.log(howManyArgs(1, false, "hello")); // -> 3
 console.log(howManyArgs("better")); // -> 1
+
+//2
+const howManyArgsArrow = (...args) => args.length;
+console.log(howManyArgsArrow()); // -> 0
+console.log(howManyArgsArrow(1, false, "hello")); // -> 3
+console.log(howManyArgsArrow("better")); // -> 1
--- sum.js
+++ sum.js
@@ -1,6 +1,11 @@
 // Create a function sum which accepts any amount of numbers and return the sum of their addition
-
-const sum = () => {};
+function sum() {
+  var sum = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
 
 console.log(sum(1)); // -> 1
 console.log(sum(1, 15)); // -> 16
